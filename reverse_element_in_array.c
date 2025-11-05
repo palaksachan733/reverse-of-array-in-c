2fc820ad6353e591c0465058534710c6d920b6e9
palak sachan <palakssachan@gmail.com>
2025-11-05 21:18:46 +0530

    Add files via upload

---
+++ reverse_element_in_array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int arr[5],i;
+    for(i=0;i<=4;i++){
+        printf("enter the element %d\n",i+1);
+        scanf("%d",&arr[i]);
+    }
+    for(i=4;i>0;i--){
+        printf("%d",arr[i]);
+    }
+    return(0);
+}
